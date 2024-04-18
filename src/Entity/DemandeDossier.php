b5587ccbfa9f9520d5aa59fb843f847c841aeb3d
GitHackerz <mohamedhabiballah.bibani@esprit.tn>
2024-04-18 09:25:29 +0100

    add entities

---
+++ src/Entity/DemandeDossier.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DemandeDossierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DemandeDossierRepository::class)]
+class DemandeDossier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urlcin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urlcerretenu = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urlatttravail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urldecrevenu = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urlextnaissance = null;
+
+    #[ORM\OneToMany(targetEntity: Dossier::class, mappedBy: 'demandeDossier')]
+    private Collection $dossier;
+
+    public function __construct()
+    {
+        $this->dossier = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrlcin(): ?string
+    {
+        return $this->urlcin;
+    }
+
+    public function setUrlcin(string $urlcin): static
+    {
+        $this->urlcin = $urlcin;
+
+        return $this;
+    }
+
+    public function getUrlcerretenu(): ?string
+    {
+        return $this->urlcerretenu;
+    }
+
+    public function setUrlcerretenu(string $urlcerretenu): static
+    {
+        $this->urlcerretenu = $urlcerretenu;
+
+        return $this;
+    }
+
+    public function getUrlatttravail(): ?string
+    {
+        return $this->urlatttravail;
+    }
+
+    public function setUrlatttravail(string $urlatttravail): static
+    {
+        $this->urlatttravail = $urlatttravail;
+
+        return $this;
+    }
+
+    public function getUrldecrevenu(): ?string
+    {
+        return $this->urldecrevenu;
+    }
+
+    public function setUrldecrevenu(string $urldecrevenu): static
+    {
+        $this->urldecrevenu = $urldecrevenu;
+
+        return $this;
+    }
+
+    public function getUrlextnaissance(): ?string
+    {
+        return $this->urlextnaissance;
+    }
+
+    public function setUrlextnaissance(string $urlextnaissance): static
+    {
+        $this->urlextnaissance = $urlextnaissance;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Dossier>
+     */
+    public function getDossier(): Collection
+    {
+        return $this->dossier;
+    }
+
+    public function addDossier(Dossier $dossier): static
+    {
+        if (!$this->dossier->contains($dossier)) {
+            $this->dossier->add($dossier);
+            $dossier->setDemandeDossier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDossier(Dossier $dossier): static
+    {
+        if ($this->dossier->removeElement($dossier)) {
+            // set the owning side to null (unless already changed)
+            if ($dossier->getDemandeDossier() === $this) {
+                $dossier->setDemandeDossier(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Dossier.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DossierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DossierRepository::class)]
+class Dossier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $cin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $region = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $montant = null;
+
+    #[ORM\ManyToOne(inversedBy: 'dossier')]
+    private ?DemandeDossier $demandeDossier = null;
+
+    public function __construct()
+    {
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCin(): ?int
+    {
+        return $this->cin;
+    }
+
+    public function setCin(int $cin): static
+    {
+        $this->cin = $cin;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): static
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): static
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDemandeDossier(): ?DemandeDossier
+    {
+        return $this->demandeDossier;
+    }
+
+    public function setDemandeDossier(?DemandeDossier $demandeDossier): static
+    {
+        $this->demandeDossier = $demandeDossier;
+
+        return $this;
+    }
+}
